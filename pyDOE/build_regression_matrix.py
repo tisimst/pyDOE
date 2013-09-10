fd77667643bf9b83bab912ceac6da93d86923236
tisimst <tisimst@gmail.com>
2013-09-10 17:53:14 -0600

    added build_regression_matrix.py and var_regression_matrix.py

---
+++ pyDOE/build_regression_matrix.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+def grep(haystack, needle):
+    start = 0
+    while True:
+        start = haystack.find(needle, start)
+        if start==-1 return
+        yield start
+        start += len(needle)
+
+def build_regression_matrix(H, model, build=None):
+    """
+    Build a regression matrix using a DOE matrix and a list of monomials.
+    
+    Parameters
+    ----------
+    H : 2d-array
+    model : str
+    build : bool-array
+    
+    Returns
+    -------
+    R : 2d-array
+    
+    """
+    ListOfTokens = model.split(' ')
+    if H.shape[1]==1:
+        size_index = len(str(H.shape[0]))
+    else:
+        size_index = len(str(H.shape[1]))
+    
+    if build is None:
+        build = [True]*len(ListOfTokens)
+    
+    # Test if the vector has the wrong direction (lines instead of columns)
+    if H.shape[0]==1:
+        H = H.T
+    
+    # Collect the list of monomials
+    Monom_Index = []
+    for i in xrange(len(ListOfTokens)):
+        if build[i]:
+            Monom_Index += [grep(ListOfTokens, 'x' + str(0)*(size_index - \
+            len(str(i))) + str(i))]
+    
+    Monom_Index = -np.sort(-Monom_Index)
+    Monom_Index = np.unique(Monom_Index)
+    
+    if H.shape[1]==1:
+        nb_var = H.shape[0]  # vector "mode": the number of vars is equal to the number of lines of H
+        VectorMode = True
+        
+        for i in xrange(nb_var):
+            for j in xrange(ListOfTokens.shape[0]):
+                ListOfTokens[j] = ListOfTokens[j].replace(
+                    'x' + str(0)*(size_index - len(str(i))) + str(i),
+                    'H(' + str(i) + ')')
+    else:
+        nb_var = H.shape[0]  # matrix "mode": the number of vars is equal to the number of columns of H
+        VectorMode = False
+        
+        for i in xrange(nb_var):
+            for j in xrange(ListOfTokens.shape[0])
+                ListOfTokens[j] = ListOfTokens[j].replace(
+                    'x' + str(0)*(size_index - len(str(i))) + str(i),
+                    'H[i,' + str(i) + ')')
+    
+    # Now build the regression matrix
+    if VectorMode:
+        R = np.zeros((len(ListOfTokens), 1))
+        for j in xrange(len(ListOfTokens)):
+            R[j, 0] = eval(ListOfTokens[j])
+    else:
+        R = np.zeros((H.shape[0], len(ListOfTokens)))
+        for i in xrange(H.shape[0]):
+            for j in xrange(len(ListOfTokens)):
+                R[i, j] = eval(ListOfTokens[j])
+    
+    return R
+    
+    
---
+++ pyDOE/var_regression_matrix.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def var_regression_matrix(H, x, model, sigma=1):
+    """
+    Compute the variance of the 'regression error'.
+    
+    Parameters
+    ----------
+    H : 2d-array
+        The regression matrix
+    x : 2d-array
+        The coordinates to calculate the regression error variance at.
+    model : str
+        A string of tokens that define the regression model (e.g. 
+        '1 x1 x2 x1*x2')
+    sigma : scalar
+        An estimate of the variance (default: 1).
+    
+    Returns
+    -------
+    var : scalar
+        The variance of the regression error, evaluated at ``x``.
+        
+    """
+    x = np.atleast_2d(x)
+    H = np.atleast_2d(H)
+    
+    if x.shape[0]==1:
+        x = x.T
+    
+    if np.rank(H)<(np.dot(H.T, H)).shape[0]:
+        raise ValueError, "model and DOE don't suit together"
+    
+    x_mod = build_regression_matrix(x, model)
+    var = sigma**2*np.dot(np.dot(x_mod.T, np.linalg.inv(np.dot(H.T, H))), x_mod)
+    return var
