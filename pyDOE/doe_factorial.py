70772f763a7e8d018e5ba75d43c924540bd13c62
tisimst <tisimst@gmail.com>
2013-09-11 17:38:31 -0600

    added fracfact function

--- pyDOE/doe_factorial.py
+++ pyDOE/doe_factorial.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 
 def fullfact(levels):
@@ -22,6 +23,8 @@ def fullfact(levels):
         >>> fullfact([2, 4, 3])
 
     """
+    levels = levels[::-1]
+    
     n = len(levels)  # number of factors
     nb_lines = np.prod(levels)  # number of trial conditions
     H = np.zeros((nb_lines, n))
@@ -37,8 +40,10 @@ def fullfact(levels):
         level_repeat *= levels[i]
         H[:, i] = rng
      
-    return H
+    return np.fliplr(H)
     
+################################################################################
+
 def ff2n(n):
     """
     Create a 2-Level full-factorial design
@@ -61,3 +66,115 @@ def ff2n(n):
     
     """
     return fullfact([2]*n)
+
+################################################################################
+
+def fracfact(gen):
+    """
+    Create a 2-level fractional-factorial design with a generator string.
+    
+    Parameters
+    ----------
+    gen : str
+        A string, consisting of lowercase, uppercase letters or operators "-"
+        and "+", indicating the factors of the experiment
+    
+    Returns
+    -------
+    H : 2d-array
+        A m-by-n matrix, the fractional factorial design. m is 2^k, where k
+        is the number of letters in ``gen``, and n is the total number of
+        entries in ``gen``.
+    
+    Notes
+    -----
+    In ``gen`` we define the main factors of the experiment and the factors
+    whose levels are the products of the main factors. For example, if
+    
+        gen = "a b ab"
+    
+    then "a" and "b" are the main factors, while the 3rd factor is the product
+    of the first two. If we input uppercase letters in ``gen``, we get the same
+    result. We can also use the operators "+" and "-" in ``gen``.
+    
+    For example, if
+    
+        gen = "a b -ab"
+    
+    then the 3rd factor is the opposite of the product of "a" and "b".
+    
+    The output matrix includes the two level full factorial design, built by
+    the main factors of ``gen``, and the products of the main factors. The
+    columns of ``H`` follow the sequence of ``gen``.
+    
+    For example, if
+    
+        gen = "a b ab c"
+    
+    then columns H[:, 0], H[:, 1], and H[:, 3] include the two level full
+    factorial design and H[:, 2] includes the products of the main factors.
+    
+    Examples
+    --------
+    ::
+    
+        H = fracfact("a b ab")
+        
+        H = fracfact("A B AB")
+        
+        H = fracfact("a b -ab c +abc")
+    
+    """
+    # Recognize letters and combinations
+    A = [item for item in re.split('\-?\s?\+?', gen) if item]  # remove empty strings
+    C = [len(item) for item in A]
+    
+    # Indices of single letters (main factors)
+    I = [i for i, item in enumerate(C) if item==1]
+    
+    # Indices of letter combinations (we need them to fill out H2 properly).
+    J = [i for i, item in enumerate(C) if item!=1]
+    
+    # Check if there are "-" or "+" operators in gen
+    U = [item for item in gen.split(' ') if item]  # remove empty strings
+    
+    # If R1 is either None or not, the result is not changed, since it is a
+    # multiplication of 1.
+    R1 = _grep(U, '+')
+    R2 = _grep(U, '-')
+    
+    # Fill in design with two level factorial design
+    H1 = 2*ff2n(len(I)) - 1
+    H = np.zeros((H1.shape[0], len(C)))
+    H[:, I] = H1
+    
+    # Recognize combinations and fill in the rest of matrix H2 with the proper
+    # products
+    for k in J:
+        # For lowercase letters
+        xx = np.array([ord(c) for c in A[k]]) - 97
+        
+        # For uppercase letters
+        if np.any(xx<0):
+            xx = np.array([ord(c) for c in A[k]]) - 65
+        
+        H[:, k] = np.prod(H1[:, xx], axis=1)
+    
+    # Update design if gen includes "-" operator
+    if R2:
+        H[:, R2] *= -1
+        
+    # Return the fractional factorial design
+    return (1 + H)/2
+    
+def _grep(haystack, needle):
+    try:
+        haystack[0]
+    except (TypeError, AttributeError):
+        return [0] if needle in haystack else []
+    else:
+        locs = []
+        for idx, item in enumerate(haystack):
+            if needle in item:
+                locs += [idx]
+        return locs
