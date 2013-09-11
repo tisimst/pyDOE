0bd5dc99dc6f55169eff13f3dbf119df2752239e
tisimst <tisimst@gmail.com>
2013-09-11 17:40:22 -0600

    added imports to doe_form and doe_plackett_burnam

--- pyDOE/__init__.py
+++ pyDOE/__init__.py
@@ -8,9 +8,11 @@ Copyright: 2013
 
 """
 __author__ = 'Abraham Lee'
-__version__ = '0.1'
+__version__ = '0.2'
 
 from doe_box_behnken import *
 from doe_composite import *
 from doe_factorial import *
 from doe_lhs import *
+from doe_fold import *
+from doe_plackett_burnam import *
