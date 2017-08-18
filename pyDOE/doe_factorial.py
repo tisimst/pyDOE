42bd6a8aca03de1e4c8cfc196ee193450a753354
Eduardo Valle <dovalle@dca.fee.unicamp.br>
2017-08-18 20:05:32 -0300

    Optimizer / analyser without considering signs on contrast

--- pyDOE/doe_factorial.py
+++ pyDOE/doe_factorial.py
@@ -6,14 +6,18 @@ Scilab:
     Copyright (C) 2010 - 2011 - INRIA - Michael Baudin
     Copyright (C) 2009 - Yann Collette
     Copyright (C) 2009 - CEA - Jean-Marc Martinez
-    
+
     website: forge.scilab.org/index.php/p/scidoe/sourcetree/master/macros
 
-Much thanks goes to these individuals. It has been converted to Python by 
+Much thanks goes to these individuals. It has been converted to Python by
 Abraham Lee.
 """
 
+import imp
+import itertools
+import math
 import re
+import string
 import numpy as np
 
 __all__ = ['np', 'fullfact', 'ff2n', 'fracfact']
@@ -21,22 +25,22 @@ __all__ = ['np', 'fullfact', 'ff2n', 'fracfact']
 def fullfact(levels):
     """
     Create a general full-factorial design
-    
+
     Parameters
     ----------
     levels : array-like
         An array of integers that indicate the number of levels of each input
         design factor.
-    
+
     Returns
     -------
     mat : 2d-array
         The design matrix with coded levels 0 to k-1 for a k-level factor
-    
+
     Example
     -------
     ::
-    
+
         >>> fullfact([2, 4, 3])
         array([[ 0.,  0.,  0.],
                [ 1.,  0.,  0.],
@@ -62,12 +66,12 @@ def fullfact(levels):
                [ 1.,  2.,  2.],
                [ 0.,  3.,  2.],
                [ 1.,  3.,  2.]])
-               
+
     """
     n = len(levels)  # number of factors
     nb_lines = np.prod(levels)  # number of trial conditions
     H = np.zeros((nb_lines, n))
-    
+
     level_repeat = 1
     range_repeat = np.prod(levels)
     for i in range(n):
@@ -78,29 +82,29 @@ def fullfact(levels):
         rng = lvl*range_repeat
         level_repeat *= levels[i]
         H[:, i] = rng
-     
+
     return H
-    
+
 ################################################################################
 
 def ff2n(n):
     """
     Create a 2-Level full-factorial design
-    
+
     Parameters
     ----------
     n : int
         The number of factors in the design.
-    
+
     Returns
     -------
     mat : 2d-array
         The design matrix with coded levels -1 and 1
-    
+
     Example
     -------
     ::
-    
+
         >>> ff2n(3)
         array([[-1., -1., -1.],
                [ 1., -1., -1.],
@@ -110,7 +114,7 @@ def ff2n(n):
                [ 1., -1.,  1.],
                [-1.,  1.,  1.],
                [ 1.,  1.,  1.]])
-       
+
     """
     return 2*fullfact([2]*n) - 1
 
@@ -119,64 +123,64 @@ def ff2n(n):
 def fracfact(gen):
     """
     Create a 2-level fractional-factorial design with a generator string.
-    
+
     Parameters
     ----------
     gen : str
         A string, consisting of lowercase, uppercase letters or operators "-"
         and "+", indicating the factors of the experiment
-    
+
     Returns
     -------
     H : 2d-array
         A m-by-n matrix, the fractional factorial design. m is 2^k, where k
         is the number of letters in ``gen``, and n is the total number of
         entries in ``gen``.
-    
+
     Notes
     -----
     In ``gen`` we define the main factors of the experiment and the factors
     whose levels are the products of the main factors. For example, if
-    
+
         gen = "a b ab"
-    
+
     then "a" and "b" are the main factors, while the 3rd factor is the product
     of the first two. If we input uppercase letters in ``gen``, we get the same
     result. We can also use the operators "+" and "-" in ``gen``.
-    
+
     For example, if
-    
+
         gen = "a b -ab"
-    
+
     then the 3rd factor is the opposite of the product of "a" and "b".
-    
+
     The output matrix includes the two level full factorial design, built by
     the main factors of ``gen``, and the products of the main factors. The
     columns of ``H`` follow the sequence of ``gen``.
-    
+
     For example, if
-    
+
         gen = "a b ab c"
-    
+
     then columns H[:, 0], H[:, 1], and H[:, 3] include the two level full
     factorial design and H[:, 2] includes the products of the main factors.
-    
+
     Examples
     --------
     ::
-    
+
         >>> fracfact("a b ab")
         array([[-1., -1.,  1.],
                [ 1., -1., -1.],
                [-1.,  1., -1.],
                [ 1.,  1.,  1.]])
-       
+
         >>> fracfact("A B AB")
         array([[-1., -1.,  1.],
                [ 1., -1., -1.],
                [-1.,  1., -1.],
                [ 1.,  1.,  1.]])
-        
+
         >>> fracfact("a b -ab c +abc")
         array([[-1., -1., -1., -1., -1.],
                [ 1., -1.,  1., -1.,  1.],
@@ -186,50 +190,50 @@ def fracfact(gen):
                [ 1., -1.,  1.,  1., -1.],
                [-1.,  1.,  1.,  1., -1.],
                [ 1.,  1., -1.,  1.,  1.]])
-       
+
     """
     # Recognize letters and combinations
     A = [item for item in re.split('\-?\s?\+?', gen) if item]  # remove empty strings
     C = [len(item) for item in A]
-    
+
     # Indices of single letters (main factors)
     I = [i for i, item in enumerate(C) if item==1]
-    
+
     # Indices of letter combinations (we need them to fill out H2 properly).
     J = [i for i, item in enumerate(C) if item!=1]
-    
+
     # Check if there are "-" or "+" operators in gen
     U = [item for item in gen.split(' ') if item]  # remove empty strings
-    
+
     # If R1 is either None or not, the result is not changed, since it is a
     # multiplication of 1.
     R1 = _grep(U, '+')
     R2 = _grep(U, '-')
-    
+
     # Fill in design with two level factorial design
     H1 = ff2n(len(I))
     H = np.zeros((H1.shape[0], len(C)))
     H[:, I] = H1
-    
+
     # Recognize combinations and fill in the rest of matrix H2 with the proper
     # products
     for k in J:
         # For lowercase letters
         xx = np.array([ord(c) for c in A[k]]) - 97
-        
+
         # For uppercase letters
         if np.any(xx<0):
             xx = np.array([ord(c) for c in A[k]]) - 65
-        
+
         H[:, k] = np.prod(H1[:, xx], axis=1)
-    
+
     # Update design if gen includes "-" operator
     if R2:
         H[:, R2] *= -1
-        
+
     # Return the fractional factorial design
     return H
-    
+
 def _grep(haystack, needle):
     try:
         haystack[0]
@@ -241,3 +245,121 @@ def _grep(haystack, needle):
             if needle in item:
                 locs += [idx]
         return locs
+
+################################################################################
+
+
+def fracfact_opt(n_factors, n_erased) :
+    """
+    Find the optimal generator string for a 2-level fractional-factorial design
+    with the specified number of factors and erased factors.
+
+    Parameters
+    ----------
+    n_factors : int
+        The number of factors in the full factorial design
+    n_erased : int
+        The number of factors to "remove" to create the fractional design
+
+    Returns
+    -------
+    gen : str
+        A generator string in the format expected by fracfact() with the 2^k-p
+        design, where k=n_factors and p=n_erased. The design disallows aliasing
+        of main factors, and minimizes aliasing of low-order interactions.
+    alias_map : list of str
+        The map of aliases that the design inflicts. Each string in the list is
+        a set of factors and interactions that are aliased among themselves, in
+        the format a = bcd = def etc. If there is no aliasing (n_erased=0) the
+        map simply lists all factors and interactions.
+    alias_vector : 1d numpy.array
+        The vector with the cost of the design in term of aliasings. Each cell
+        in the array counts the number of aliasing between factors/interactions
+        of size i and of size j, as given by alias_vector_indices().
+    """
+def fracfact_opt(n_factors, n_erased) :
+    def n_comb(n, k):
+        if k<=0 or n<=0 or k>n: return 0
+        return math.factorial(n) / (math.factorial(k) * math.factorial(n-k))
+    if n_factors > 20:
+        raise ValueError('Design too big, use 20 factors or less')
+    if n_factors<2:
+        raise ValueError('Design too small')
+    if n_erased<0:
+        raise ValueError('Number of erased factors must be non-negative')
+    n_main_factors = n_factors-n_erased
+    n_aliases = sum(( n_comb(n_main_factors, n) for n in range(2, n_main_factors+1) ))
+    if n_erased>n_comb(n_aliases, n_erased):
+        raise ValueError('Too many erased factors to create aliasing')
+    all_names = string.ascii_lowercase
+    factors = range(n_factors)
+    main_factors = range(n_main_factors)
+    main_design = ' '.join([ all_names[f] for f in main_factors ])
+    aliases = itertools.chain.from_iterable(( itertools.combinations(main_factors, n) for n in range(2, n_main_factors+1) ))
+    aliases = sorted(list(aliases), key=lambda a : (len(a), a), reverse=True)
+    best_design = None
+    best_map = []
+    best_vector = np.repeat(n_factors, n_factors)
+    design_shape = (2**n_main_factors, n_factors)
+    for aliasing in itertools.combinations(aliases, n_erased):
+        aliasing_design = ' '.join([ ''.join([ all_names[f] for f in a ]) for a in aliasing ])
+        complete_design = main_design+' '+aliasing_design
+        # print(complete_design)
+        design = fracfact(complete_design)
+        assert design.shape == design_shape
+        alias_map, alias_vector = fracfact_aliasing(design)
+        # print(_)
+        # print(alias_vector)
+        if list(alias_vector) < list(best_vector):
+            best_design = complete_design
+            best_map = alias_map
+            best_vector = alias_vector
+            # print('Best design so far:', complete_design, 'with vector:', list(best_vector))
+    return best_design, best_map, best_vector
+
+def fracfact_aliasing(design):
+    n_rounds, n_factors = design.shape
+    if n_factors > 20:
+        raise ValueError('Design too big, use 20 factors or less')
+    all_names = string.ascii_lowercase
+    factors = range(n_factors)
+    all_combinations = itertools.chain.from_iterable(( itertools.combinations(factors, n) for n in range(1, n_factors+1) ))
+    aliases = {}
+    for combination in all_combinations:
+        contrast = np.prod(design[:,combination], axis=1)
+        contrast.flags.writeable = False
+        aliases[contrast.data] = aliases.get(contrast.data, [])
+        aliases[contrast.data].append(combination)
+    aliases_list = []
+    for alias in aliases.values():
+        aliases_list.append(sorted(alias, key=lambda a : (len(a), a)))
+    aliases_list = sorted(aliases_list, key=lambda list : ([len(a) for a in list], list))
+    aliases_readable = []
+    alias_matrix = np.zeros((n_factors, n_factors, ))
+    # print(aliases_list)
+    # print([ len(a) for a in aliases_list ])
+    for alias in aliases_list:
+        alias_readable = ' = '.join([ ''.join([ all_names[f] for f in a ]) for a in alias ])
+        aliases_readable.append(alias_readable)
+        for sizes in itertools.combinations([ len(a) for a in alias], 2):
+            assert sizes[0]>=0 and sizes[1]>=0
+            assert sizes[0] <= sizes[1]
+            alias_matrix[sizes[0]-1, sizes[1]-1] += 1
+    alias_vector = alias_matrix[alias_vector_indices(n_factors)]
+    return aliases_readable, alias_vector
+
+def design_frac(n_factors, n_erase):
+    design, alias_map, alias_vector = fracfact_opt(n_factors, n_erase)
+    alias_matrix = np.zeros((n_factors, n_factors,))
+    alias_matrix[alias_vector_indices(n_factors)] = alias_vector
+    return design, alias_map, alias_matrix
+
+def alias_vector_indices(n_factors):
+    if n_factors > 20:
+        raise ValueError('Design too big, use 20 factors or less')
+    n_factors = 6
+    indices = list(itertools.combinations_with_replacement(range(n_factors), 2))
+    indices = sorted(indices, key=lambda i: max(i))
+    rows = np.asarray([ i[0] for i in indices ])
+    cols = np.asarray([ i[1] for i in indices ])
+    return rows, cols
